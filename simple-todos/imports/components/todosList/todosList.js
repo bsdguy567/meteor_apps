4d65c5052f6775383d8a060dcb724746a1b3a6d3
kameron90d <kameron90d@yahoo.com>
2018-01-15 16:51:37 -0600

    working with users

--- simple-todos/imports/components/todosList/todosList.js
+++ simple-todos/imports/components/todosList/todosList.js
@@ -1,5 +1,7 @@
+
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
+import { Meteor } from 'meteor/meteor';
 import { Tasks } from '../../api/tasks.js';
 
 import template from './todosList.html';
@@ -13,10 +15,6 @@ class TodosListCtrl {
         this.helpers({
             tasks() {
                 const selector = {};
-                //The query that returns all tasks(the current query looks like that:
-                //  Tasks.find({}, { sort: { createdAt: -1 } }
-                //and the query to return only the not completed todos looks like that:
-                //  Tasks.find({ checked: { $ne: true } }, { sort: { createdAt: -1 } })
 
                 // If hide completed is checked, filter tasks
                 if (this.getReactively('hideCompleted')) {
@@ -26,27 +24,37 @@ class TodosListCtrl {
                 }
 
                 // Show newest tasks at the top
-                return Tasks.find(selector, {}, {
+                return Tasks.find(selector, {
                     sort: {
                         createdAt: -1
                     }
                 });
+            },
+            incompleteCount() {
+                return Tasks.find({
+                    checked: {
+                        $ne: true
+                    }
+                }).count();
+            },
+            currentUser() {
+                return Meteor.user();    
+            }
         })
     }
 
-
     addTask(newTask) {
         // Insert a task into the collection
-        Tasks.insert({text: newTask, createdAt: new Date});
+        Tasks.insert({
+            text: newTask,
+            createdAt: new Date,
+            owner: Meteor.userId(),
+            username: Meteor.user().username
+        });
 
         // Clear form
         this.newTask = '';
     }
-    
-    //To watch checked state of each task.
-    //  ng-checked="task.checked"
-    //And to change current state by calling setChecked method of the controller.
-    //  ng-click="$ctrl.setChecked(task)"
 
     setChecked(task) {
         // Set the checked property to the opposite of its current value
@@ -62,12 +70,11 @@ class TodosListCtrl {
     }
 }
 
-
-
 export default angular.module('todosList', [
     angularMeteor
 ])
     .component('todosList', {
         templateUrl: 'imports/components/todosList/todosList.html',
-        controller: TodosListCtrl
+        controller: ['$scope', TodosListCtrl]
     });
+
